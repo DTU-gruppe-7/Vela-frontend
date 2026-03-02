2966650e5ece4379708e4296584efed6f15bf92b
JeppeRohde <s246221@student.dtu.dk>
2026-03-02 16:02:08 +0100

    V0 Shoppinglist

--- src/navigation/AppRouter.tsx
+++ src/navigation/AppRouter.tsx
@@ -3,6 +3,7 @@ import SwipePage from '../features/swipe/SwipePage'
 import GroupPage from '../features/groups/GroupPage'
 import ProfilePage from '../features/profile/ProfilePage'
 import RecipePage from '../features/recipes/RecipePage'
+import ShoppingListPage from '../features/shopping/ShoppingListPage'
 
 
 function AppRouter() {
@@ -12,6 +13,7 @@ function AppRouter() {
             <Route path="/groups" element={<GroupPage />} />
             <Route path="/profile" element={<ProfilePage />} />
             <Route path="/recipes" element={<RecipePage />} />
+            <Route path="/shoppinglist" element={<ShoppingListPage />} />
             <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
     )
